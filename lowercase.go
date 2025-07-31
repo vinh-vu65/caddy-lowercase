7fdaa2042bd99c129c61ece419a369dd46b0d433
Vinh Vu <vinh.vu@august.com.au>
2025-07-31 12:08:03 +1000

    add lowercase logic

--- lowercase.go
+++ lowercase.go
@@ -40,8 +40,13 @@ func (m *Middleware) Validate() error {
 func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	lowerPath := strings.ToLower(r.URL.Path)
 	if r.URL.Path != lowerPath {
-		// Rewrites the path to lowercase, preserving query string
-		r.URL.Path = lowerPath
+		// Redirect to lowercase path, preserving query string
+		newURL := lowerPath
+		if r.URL.RawQuery != "" {
+			newURL += "?" + r.URL.RawQuery
+		}
+		http.Redirect(w, r, newURL, http.StatusMovedPermanently)
+		return nil
 	}
 	return next.ServeHTTP(w, r)
 }
